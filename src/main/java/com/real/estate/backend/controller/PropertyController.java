ce3c9ea8ba55b2e6b4e0d9df75f54cb91f463804
l33t-c0d3r-66 <aliraza.arain.12530@gmail.com>
2023-10-04 20:38:16 +0500

    Build Public and private API for Property Information

---
+++ src/main/java/com/real/estate/backend/controller/PropertyController.java
@@ -0,0 +1,50 @@
+package com.real.estate.backend.controller;
+
+import com.real.estate.backend.model.PropertyInformation;
+import com.real.estate.backend.service.PropertyInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PropertyController {
+
+    @Autowired
+    private PropertyInformationService propertyInformationService;
+
+
+    @GetMapping("/api/public/properties/{type}")
+    public List<PropertyInformation> getPropertiesByType(@PathVariable("type") String type) {
+        List<PropertyInformation> properties = propertyInformationService.getPropertiesByType(type);
+        if(properties != null) {
+            return properties;
+        }
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/api/public/property/{id}")
+    public PropertyInformation getPropertyById(@PathVariable("id") Long id) {
+        return propertyInformationService.getPropertyInformation(id);
+    }
+
+    @PostMapping("/api/v1/property")
+    public PropertyInformation saveProperty(@RequestBody PropertyInformation propertyInformation) {
+        return propertyInformationService.save(propertyInformation);
+    }
+
+    @PutMapping("/api/v1/property/{id}")
+    public PropertyInformation updateProperty(@PathVariable("id") Long id, PropertyInformation propertyInformation) {
+        return propertyInformationService.updatePropertyInformation(id, propertyInformation);
+    }
+
+    @DeleteMapping("/api/v1/property/{id}")
+    public String deletePropertyInformation(@PathVariable("id") Long id) {
+        Boolean isDeleted = propertyInformationService.deletePropertyById(id);
+        if(isDeleted) {
+            return "Property Deleted Successfully";
+        }
+        return "Failed to Delete the Property";
+    }
+}
